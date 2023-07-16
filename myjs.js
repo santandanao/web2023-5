5c3fd5fd7cf6c48cf1d66920a3051510bd5db0c3
santandanao <131091831+santandanao@users.noreply.github.com>
2023-07-16 20:15:09 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,44 +1,6 @@
-var names;
-var divElem;
-
-//ページ読み込み時の処理の関数
-function loadPage(){
-  names=getCookie("NAME");//クッキーから名前を取得して変数namesに代入
-  divElem=document.getElementById("div");
-
-  if(name == ""){ //クッキーから取得できなかった場合
-    divElem.innerHTML="<p>お名前は？<input type='text' id='name'/></p>"+
-      "<p><input type='button' value='OK' onclick='clickOK()'/></p>";
-
-  }else{ //取得できた場合
-    divElem.innerHTML="<b>ようこそ"+names+"さん！</b>";
-  }
-
-//OKボタンがクリックされたとき呼び出される関数
-function clickOK(){
-  setCookie("NAME",document.getElementById("name").value);
-  loadPage();
+function showTime(){
+  var formElem=document.getElementById("form1");
+  today=new Date();
+  formElem.output1.value=today.toLocaleString();
 }
 
-//クッキーから値を読み込む関数
-function getCookie(keyname){
-
-  var tmp=document.cookie+";";
-  var index1=tmp.index0f(keyname,0);
-
-  if(index1 !=-1){
-    tmp=tmp.substring(index1, tmp.length);
-    var index2=tmp.index0f("=",0);
-    var index3=tmp.index0f(";",index2);
-    return unescape(tmp.substring(index2+1,index3));
-  }
-  return"";
-}
-
-//クッキーに値を書き込む関数
-function setCookie(keyname,val){
-  var tmp=kayname+"="+escape(val)+";";
-  tmp+="expires=Sun,31-Mar-2024 23:59:59;";
-  document.cookie=tmp;
-}
-  
